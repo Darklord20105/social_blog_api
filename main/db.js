aa6b2d782c32f65abe00f770499910e071be8374
unknown <omarhj1992@gmail.com>
2020-06-08 22:58:24 +0300

    nextfix 002

--- main/db.js
+++ main/db.js
@@ -2,6 +2,16 @@ const { Pool } = require("pg")
 require('dotenv').config()
 const isProduction = process.env.NODE_ENV === 'production'
 
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
+
+const pool = new Pool({
+    connectionString: isProduction ? process.env.DATABASE_URL : connectionString
+})
+
+
+module.exports = pool
+
+// local db info
 // const pool = new Pool({
 //     user: "postgres",
 //     host: "localhost",
@@ -9,17 +19,7 @@ const isProduction = process.env.NODE_ENV === 'production'
 //     password: "darklord",
 //     port: 5432
 // })
+// you should modify th packge.json file in the proxy section to the right url (in local case http://localhost:5000)
 
-const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
-
-const pool = new Pool({
-    // user: process.env.DB_USER,
-    // host: process.env.DB_HOST,
-    // database: process.env.DB_DATABASE,
-    // password: process.env.DB_PASSWORD,
-    // port: process.env.DB_PORT
-    connectionString: isProduction ? process.env.DATABASE_URL : connectionString
-})
-
-
-module.exports = pool
+// access database from server
+// heroku pg:psql postgresql-regular-57507 --app my-social-blog-api-server
